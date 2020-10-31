9f2cfdae53eb6e9cfa9003206a5f94d9467b3078
Isaac <isaacgc08@gmail.com>
2020-10-30 20:47:26 -0700

    Created test for App and EventList

--- src/EventList.js
+++ src/EventList.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
+import Event from './Event';
 
 class EventList extends Component {
+  state = {
+    events: [],
+  };
   render() {
-    return <div></div>;
+    return (
+      <ul className='EventList'>
+        {this.state.events.map((event) => (
+          <li key={event.id}>
+            <Event event={event} />
+          </li>
+        ))}
+      </ul>
+    );
   }
 }
 
